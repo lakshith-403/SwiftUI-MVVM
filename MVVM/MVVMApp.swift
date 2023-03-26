206b18254e241ed85c233f76c6928b4439417659
Lakshith Nishshanke <lakshith.k.nishshanke@gmail.com>
2023-03-26 08:42:21 +0530

    Project init

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by Lakshith Nishshanke on 2023-03-26.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
